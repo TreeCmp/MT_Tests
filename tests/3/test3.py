97c9544d563776e7f0dac233c46c586aa3d0c134
Tomasz Goluch <goluch@eti.pg.edu.pl>
2025-06-25 17:08:09 +0200

    added test 3 for binary trees

--- tests/3/test3.py
+++ tests/3/test3.py
@@ -6,10 +6,14 @@ import csv
 import os
 from pathlib import Path
 
-unrooted = 'u'
+unrooted = '-u'
+binary = '-b'
+arbitrary = '-a'
 
+treeCmpApp = "../../soft/TreeCmp/exe/TreeCmp-2.0-b103.jar"
 ptrGenAppName = "PRTGen.exe"
 genTreesFileDir = "all_trees"
+genTreesResFileDir = "all_trees_res"
 curDir = os.path.dirname(os.path.abspath(__file__))
 mainDir = Path(curDir).parents[1]
 softPath = mainDir / "soft"
@@ -17,13 +21,22 @@ prtGenDir = softPath / "PRTGen"
 prtGenPath = prtGenDir / ptrGenAppName
 
 genTreesFilePath = Path(curDir, genTreesFileDir)
-
+genTreesResFilePath = Path(curDir, genTreesResFileDir)
 
 
 if not genTreesFilePath.exists():
     os.makedirs(genTreesFilePath)
-for n in range(6, 9):
+if not genTreesResFilePath.exists():
+    os.makedirs(genTreesResFilePath)
+for n in range(5, 9):
 
-        treesPath = Path(genTreesFilePath, "all_n_" + str(n) + "_trees.newick")
-        print (subprocess.list2cmdline([prtGenPath, "-n", str(n), "-" + unrooted, "-f", treesPath]))
-        subprocess.check_call([prtGenPath, "-n", str(n), "-" + unrooted, "-f", treesPath])
+        binaryTreesPath = Path(genTreesFilePath, "all_b_n_" + str(n) + "_trees.newick")
+        binaryTreesResultPath = Path(genTreesResFilePath, "all_b_n_" + str(n) + "_trees.out")
+        print (subprocess.list2cmdline([prtGenPath, "-n", str(n), unrooted, binary, "-f", binaryTreesPath]))
+        subprocess.check_call([prtGenPath, "-n", str(n), unrooted, binary, "-f", binaryTreesPath])
+        print (subprocess.list2cmdline(["java", "-jar", treeCmpApp, "-m", "-d", "m3", "-i", binaryTreesPath, "-o", binaryTreesResultPath]))
+        subprocess.check_call(["java", "-jar", treeCmpApp, "-m", "-d", "m3", "-i", binaryTreesPath, "-o", binaryTreesResultPath])
+
+        # arbitraryTreesPath = Path(genTreesFilePath, "all_a_n_" + str(n) + "_trees.newick")
+        # print (subprocess.list2cmdline([prtGenPath, "-n", str(n), unrooted, arbitrary, "-f", arbitraryTreesPath]))
+        # subprocess.check_call([prtGenPath, "-n", str(n), unrooted, arbitrary, "-f", arbitraryTreesPath])
